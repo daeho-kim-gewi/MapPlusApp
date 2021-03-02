55b5a60d49b357bbea66d68ad7f534fb928cb9b9
daeho.kim <daeho.kim@gewi.com>
2021-03-02 14:31:59 +0100

    Initial Commit

---
+++ MapPlusApp/MapPlusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapPlusAppApp.swift
+//  MapPlusApp
+//
+//  Created by Daeho Kim on 02.03.21.
+//
+
+import SwiftUI
+
+@main
+struct MapPlusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
